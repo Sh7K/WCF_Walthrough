6a8ff69d03370d874c118e137fa92b595626c876
kshameed7@gmail.com <kshameed7@gmail.com>
2017-07-26 20:24:49 +0530

    CLient files

---
+++ NumericService/NumericService.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel;
+
+namespace NumericService
+{
+    [ServiceContract]
+ public   interface INumericService
+    {
+        [OperationContract]
+        int Add(int a,int b);
+        [OperationContract]
+        int Multiply(int a,int b);
+    }
+    public class NumericService : INumericService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
